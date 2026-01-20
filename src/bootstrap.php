619ca4c122418a86244a5bbaf5999e7aaa4a445a
CodeCrafter618 <511198@student.firda.nl>
2026-01-20 11:15:39 +0100

    Opdracht 4 de router

--- src/bootstrap.php
+++ src/bootstrap.php
@@ -6,29 +6,78 @@ use GuzzleHttp\Psr7\ServerRequest;
 use GuzzleHttp\Psr7\Response;
 use GuzzleHttp\Psr7\Utils;
 use HttpSoft\Emitter\SapiEmitter;
+use League\Route\Router;
 
 ini_set("display_errors", 1);
 
 require dirname(__DIR__) . "/vendor/autoload.php";
 
-$reqeust = ServerRequest::fromGlobals();
+$request = ServerRequest::fromGlobals();
 
-$page = $reqeust->getQueryParams()["page"];
+$router = new Router;
 
-ob_start();
+$router->get("/", function () {
+    $stream = Utils::streamFor("homepagina");
 
-require dirname(__DIR__) . "/{$page}.php";
+    $response = new Response();
 
-$content = ob_get_clean();
+    $response = $response->withBody($stream);
 
-$stream = Utils::streamFor($content);
+    return $response;
 
-$response = new Response();
+});
 
-$response = $response->withStatus(418)
-    ->withHeader("X-Powered-By", "PHP")
-    ->withBody($stream);
 
-$emitter = new SapiEmitter;
+$router->get("/products", function () {
+    $stream = Utils::streamFor("List of Products");
+
+    $response = new Response();
+
+    $response = $response->withBody($stream);
+
+    return $response;
+
+});
+
+$router->get("/product/{id:number}", function ($request, $args) {
+
+    $id = $args["id"];
+
+    $stream = Utils::streamFor("Single product with product ID $id");
+
+    $response = new Response();
+
+    $response = $response->withBody($stream);
+
+    return $response;
+
+});
+
+$router->get("/wegen", function () {
+    $stream = Utils::streamFor("Maak hier jouw wegen aan");
+
+    $response = new Response();
+
+    $response = $response->withBody($stream);
+
+    return $response;
+
+});
+
+$router->get("/instellingen", function () {
+    $stream = Utils::streamFor("Instelling om te regelen wat er aangepast moet worden hoevaak er gestrooid moet worden");
+
+    $response = new Response();
+
+    $response = $response->withBody($stream);
+
+    return $response;
+
+});
+
+
+$response = $router->dispatch($request);
+
+$emitter = new SapiEmitter();
 
 $emitter->emit($response);
