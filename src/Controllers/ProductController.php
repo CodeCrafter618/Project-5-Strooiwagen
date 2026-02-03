29af2eb4689fa18d1038beaa1fd6b4ff82facbde
CodeCrafter618 <511198@student.firda.nl>
2026-02-03 10:30:38 +0100

    Aangepast

--- src/Controllers/ProductController.php
+++ src/Controllers/ProductController.php
@@ -4,9 +4,14 @@ declare(strict_types=1);
 
 namespace App\Controllers;
 
+use Doctrine\DBAL\DriverManager;
+use Doctrine\ORM\EntityManager;
 use Framework\Controller\AbstractController;
 use Psr\Http\Message\ResponseInterface;
 use Psr\Http\Message\ServerRequestInterface;
+use PDO;
+use App\Entities\Product;
+use Doctrine\ORM\ORMSetup;
 
 
 class ProductController extends AbstractController
@@ -14,7 +19,32 @@ class ProductController extends AbstractController
 
     public function index(): ResponseInterface
     {
-        return $this->render("product/index");
+        $paths = [dirname(__DIR__) . "/src/Entities"];
+
+        $config = ORMSetup::createAttributeMetadataConfiguration($paths, true);
+
+
+        $params = [
+            "driver" => "pdo_mysql",
+            "host" => "localhost",
+            "dbname" => "zoutstrooimanagment",
+            "user" => "root",
+            "password" => "ServBay.dev"
+        ];
+        $connection = DriverManager::getConnection($params, $config);
+
+        $em = new EntityManager($connection, $config);
+        $repo = $em->getRepository(Product::class);
+        $products = $repo->findAll();
+
+        return $this->render(
+            "product/index",
+            [
+                "products" => $products
+            ]
+        );
+
+
 
     }
 
--- views/Product/index.php
+++ views/Product/index.php
@@ -1,3 +1,25 @@
 <?php $this->layout("layout", ["title" => "Products"]) ?>
 
 <h1>List of Products </h1>
+
+<table>
+
+    <thead>
+
+        <tr>
+            <th>Name</th>
+            <th>Description</th>
+            <th>Size</th>
+        </tr>
+    </thead>
+    <tbody>
+
+        <?php foreach ($products as $product): ?>
+            <tr>
+                <td><?= $this->e($product->getName()) ?></td>
+                <td><?= $this->e($product->getDescription()) ?></td>
+                <td><?= $product->getSize() ?></td>
+            </tr>
+        <?php endforeach; ?>
+    </tbody>
+</table>
