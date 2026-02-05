61a7ab637aa197bd30bfe5f4979f45333acb7acf
CodeCrafter618 <511198@student.firda.nl>
2026-02-05 15:11:40 +0100

    Header en footer

--- views/footer.php
+++ views/footer.php
@@ -1,3 +1,5 @@
-</body>
-
-</html>
+<footer class="footer-onder">
+    <div class="footer-content">
+        <p>&copy; <?php echo date("Y"); ?> Sudwest Fryslân Zoutstrooimanagment. Alle rechten voorbehouden.</p>
+    </div>
+</footer>
--- views/layout.php
+++ views/layout.php
@@ -4,12 +4,27 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+<<<<<<< Updated upstream
     <title><?= $this->e($title) ?></title>
 </head>
 
 <body>
 
+=======
+    <link rel="stylesheet" href="/example.css">
+    <title>
+        <?= $this->e($title) ?>
+    </title>
+    <style>
+
+    </style>
+</head>
+
+<body>
+    <?= $this->insert("header") ?>
+>>>>>>> Stashed changes
     <?= $this->section("content") ?>
+    <?= $this->insert("footer") ?>
 </body>
 
 </html>
